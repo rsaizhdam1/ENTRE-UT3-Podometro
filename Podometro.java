29172146f9a1d79a22e030866a56b46fc3066089
Rubén Saiz <rsaizhdam1@gmail.com>
2021-10-07 17:11:26 +0200

    Asignadas las constantes y los atributos

--- Podometro.java
+++ Podometro.java
@@ -3,11 +3,25 @@
  * acerca de los pasos, distancia, ..... que una persona
  * ha dado en una semana. 
  * 
- * @author    - pon aquí tu nombre - 
+ * @author  Rubén Saiz 
  */
 public class Podometro {
-    
-
+    private final char HOMBRE = 'H';
+    private final char MUJER = 'M';
+    private final double ZANCADA_HOMBRE = 0.45;
+    private final double ZANCADA_MUJER = 0.41;
+    private final int SABADO = 6;
+    private final int DOMINGO = 7;
+    private String marca;
+    private double altura;    
+    private char sexo;
+    private int totalPasosLaborables;
+    private int totalPasosSabado;
+    private int totalPasosDomingo;
+    private double totalDistanciaSemana;
+    private double totalDistanciaFinSemana;
+    private int tiempo;
+    private int caminatasNoche;
     /**
      * Inicializa el podómetro con la marca indicada por el parámetro.
      * El resto de atributos se ponen a 0 y el sexo, por defecto, es mujer
@@ -21,10 +35,8 @@ public class Podometro {
      * accesor para la marca
      *  
      */
-    public      getMarca() {
-
-         
-
+    public String getMarca() {
+        return marca;
     }
 
     /**
@@ -54,8 +66,7 @@ public class Podometro {
      *   
      *   (leer enunciado del ejercicio)
      */
-    public void registrarCaminata(int pasos, int dia, int horaInicio,
-                            int horaFin) {
+    public void registrarCaminata(int pasos, int dia, int horaInicio,int horaFin) {
 
        
 
